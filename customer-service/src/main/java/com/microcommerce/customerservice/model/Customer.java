1213008b172f0f481fa5daaae3dfe9ce5fbb82cf
Rashmi <rashmi.bhavsar02@gmail.com>
2025-07-03 11:25:20 +0200

    Creation de l'entite Customer service

---
+++ customer-service/src/main/java/com/microcommerce/customerservice/model/Customer.java
@@ -0,0 +1,19 @@
+package com.microcommerce.customerservice.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+}
